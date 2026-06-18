4a5fc519c93c1f58d9e7306cd27fc7d0bc5ea4d1
Aleksey Chaykin <barjastroy@gmail.com>
2026-06-18 11:09:41 +0300

    Initial Commit

---
+++ nebulaTestApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nebulaTestApp
+//
+//  Created by A Ch on 18.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nebulaTestApp/nebulaTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  nebulaTestAppApp.swift
+//  nebulaTestApp
+//
+//  Created by A Ch on 18.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct nebulaTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
